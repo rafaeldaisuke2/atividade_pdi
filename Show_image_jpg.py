fb623f1e0ddbe9028054cb3348ed78b544b32098
Labvis <labvis123>
2019-04-29 14:59:20 -0300

    tons de cinza finalizado

--- Show_image_jpg.py
+++ Show_image_jpg.py
@@ -1,17 +1,13 @@
 #pip install opencv-python
 
 import cv2
-#Carregar e armazanar imagem jpg
 
+#Carregar e armazanar imagem jpg
 imagem = cv2.imread("Croissant.jpg")
 
 #Exibir imagem jpg
-lab = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
 
-#gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Imagem original JPG", imagem)
 
-cv2.imshow("Imagem JPG", imagem)
-#cv2.imshow("Imagem JPG", gray)
-cv2.imshow("Imagem JPG", lab)
 cv2.waitKey(0)
 
--- escalacinza.py
+++ escalacinza.py
@@ -1,15 +1,19 @@
 import cv2
+import numpy as np
 
-# Carregar e armazanar imagem jpg
+# Carregar e armazenar imagem jpg
 
 imagem = cv2.imread("Croissant.jpg")
 
+# get b,g,r
+(canalAzul, canalVerde, canalVermelho) = cv2.split(imagem)
+
+
+for y in range(0, imagem.shape[0]): #percorre linhas
+    for x in range(0, imagem.shape[1]): #percorre colunas
+        #média aritmética dos canais RGB e arredondamento para inteiro
+        imagem[y, x] = np.round((canalAzul[y, x] + canalVerde[y, x] + canalVermelho[y, x])/3, 0)
+
 # Exibir imagem jpg
-
-#cv2.imshow("Imagem JPG", imagem)
-
-for y in range(0, imagem.shape[0]):  # percorre linhas
-    for x in range(0, imagem.shape[1]):  # percorre colunas
-        
-cv2.imshow("Imagem modificada", imagem)
+cv2.imshow("Imagem com Tom Cinza", imagem)
 cv2.waitKey(0)
